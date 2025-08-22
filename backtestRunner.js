fb3f3c6c6f8eb50a2659348b58cced0736e63122
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-08-22 04:03:02 +0200

    Update backtestRunner.js

--- backtestRunner.js
+++ backtestRunner.js
@@ -49,7 +49,7 @@ export class BacktestRunner {
 
             if (!this.executionHandler.getOpenTrade()) {
                 const signalFound = this._checkForSignal(marketData);
-                if (signalFound) {
+                if (signalFound && i % 7 === 0) {
                     if (apiCallCount >= this.config.MAX_API_CALLS) {
                         log.info(`[BACKTEST] Reached the API call limit. Ending simulation.`);
                         break;
