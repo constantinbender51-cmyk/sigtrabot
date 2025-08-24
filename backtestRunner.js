f9cf477378eeccf9f036c194e9db6d14f00f29b1
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-08-25 01:56:12 +0200

    Update backtestRunner.js

--- backtestRunner.js
+++ backtestRunner.js
@@ -45,7 +45,7 @@ export class BacktestRunner {
 
   async run() { 
     let candles = this.data.getAllCandles();
-    candles = filterByDate(candles, '2023-02-05', '2024-01-01');
+    candles = filterByDate(candles, '2022-01-01', '2024-01-01');
     if (!candles || candles.length < this.cfg.WARMUP_PERIOD) {
       throw new Error('Not enough data for the warm-up period.');
     }
